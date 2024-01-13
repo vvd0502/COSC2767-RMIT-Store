67f6382050bce5acab16fc76739ee0e86588d1e9
vvd0502 <134131182+vvd0502@users.noreply.github.com>
2024-01-13 15:58:59 +0700

    Update sql-script.sql

--- sql-scripts/sql-script.sql
+++ sql-scripts/sql-script.sql
@@ -5,7 +5,7 @@ FLUSH PRIVILEGES;
 CREATE DATABASE rmit_store_db;
 USE rmit_store_db;
 
-CREATE TABLE stores (
+CREATE TABLE store (
   id mediumint(8) unsigned NOT NULL auto_increment, 
   Name varchar(255) default NULL, 
   Price varchar(255) default NULL, 
